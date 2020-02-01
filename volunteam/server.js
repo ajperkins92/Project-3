df0094d195105d85ca0e8b19cd1689aebdbe7ab1
Kensen <hirohtk@gmail.com>
2020-02-01 11:52:29 -0800

    test for master protection

--- volunteam/server.js
+++ volunteam/server.js
@@ -19,6 +19,8 @@ app.use(routes);
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/volunteam";
 mongoose.connect(MONGODB_URI);
 
+// TEST
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
