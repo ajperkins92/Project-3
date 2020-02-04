70ef45257f48ffa121df80d4bbfe262b094f66d5
Tyler Shuck <tshuck88@gmail.com>
2020-02-04 11:51:22 -0800

    created routes to update user info and to get events a user belongs to

--- volunteam/routes/user.js
+++ volunteam/routes/user.js
@@ -2,14 +2,13 @@ const path = require("path");
 const router = require("express").Router();
 const db = require("../model");
 
-// gets all users
+// get route for all users
 router.get("/user", function (req, res) {
-    db.Users.find({}).then((response) => {
-        res.json(response);
-    });
+    db.Users.find({})
+        .then((response) => res.json(response))
 });
 
-// creates a new user
+// post route to create a new user
 router.post("/user", function (req, res) {
     console.log("api POST request received");
     let newUser = {};
@@ -22,23 +21,41 @@ router.post("/user", function (req, res) {
     newUser.image = req.body.image;
 
     db.Users.create(newUser)
-        .then((response) => {
-            console.log("test");
-            console.log(response);
-            res.json(response);
-        })
+        .then((response) => res.json(response))
         .catch(err => res.status(422).json(err));
 });
 
-// gets a specific user by id
+// get route for a specific user by id
 router.get("/user/:id", (req, res) => {
     db.Users.find({ _id: req.params.id })
-        .then(user => {
-            res.json(user)
+        .then(user => res.json(user))
+        .catch(err => res.json(err))
+});
+
+// put route to update user information
+router.put("/user/:id", (req, res) => {
+    db.Users.findByIdAndUpdate(id,
+        {
+            // users not allowed to change their username
+            // so update all fields except username
+            $set: {
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                password: req.body.password,
+                iamge: req.body.image
+            }
         })
-        .catch(err => {
-            res.json(err)
-        })
+        .then(updatedUser => res.json(updatedUser))
+        .catch(err => res.json(err))
+});
+
+// get route to get all events a user is attending or organizing
+router.get("/user/:id/myevents", (req, res) => {
+    db.Users.find({ _id: req.params.id })
+        .populate("Events")
+        .then(user => res.json(user))
+        .catch(err => res.json(err))
 });
 
 
