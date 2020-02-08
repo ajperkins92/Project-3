2abe6668faee77efe1b4cc95cc7759f9235d0811
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-07 22:54:06 -0800

    Adds a change to myevents.js in attempt to log capitalization changes

--- volunteam/src/pages/myevents.js
+++ volunteam/src/pages/myevents.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from "../components/mainpage/nav"
 import ViewMyEventsHeader from "../components/ViewMyEventsPage/ViewMyEventsHeader"
 
+// This lives here for committing purposes"
 
 class MyEvents extends React.Component {
 
