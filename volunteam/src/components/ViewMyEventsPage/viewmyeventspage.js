2591a4d75d58bc37578d909c853767dfd59de242
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-07 20:18:03 -0800

    Creates ViewMyEventsPage directory and components

---
+++ volunteam/src/components/ViewMyEventsPage/viewmyeventspage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./viewmyeventspage.css";
+
+function CreateEventForm(props) {
+    return (
+        // Arbitrary parent div
+        <div>
+
+            <div class="row">
+                <div class="col s12 m6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">My Events</span>
+                            <p>Browse the events you've either created or signed up for here!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <form class="col s6">
+                
+        
+
+                </form>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default ViewMyEventsPage;
