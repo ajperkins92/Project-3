f0a47d88ad341e251f4c3bc63d365d5decb4457c
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2020-02-08 14:00:46 -0800

    log in react page with style

--- client/src/components/LogInPage/logInPage.js
+++ client/src/components/LogInPage/logInPage.js
@@ -1,39 +1,46 @@
 import React from "react";
-import "./logInPage.css";
+//import "./logInPage.css";
 
 function LogInPageComponent() {
     return (
         <div className="container">
-            <div className="row subContainer">
-                <form className="col s12">
-                    <div className="row">
-                        <div className="col s12">
-                            <div className="card">
-                                <span className="card-title">Log In</span>
-                                <div className="card-image">
-                                    <img src={"https://raw.github.com/ajperkins92/Project-3/master/client/public/images/seattlePark2.jpg"} style={{ width: "90%" }} />
-                                </div>
-                                <div className="card-content">
-                                    <div className="row">
-                                        <div className="input-field col s12">
-                                            <input id="username" type="text" className="validate" />
-                                            <label for="username">Username</label>
+            <div class="row">
+                <div class="col s12">
+                    <div class="card-panel">
+                        <div className="row subContainer">
+                            <form className="col s12">
+                                <div className="row">
+                                    <div className="col s12">
+                                        <span className="card-title" style={{ fontSize: "3rem" }}>Log In</span>
+                                        <div className="card-image">
+                                            <img src={"https://raw.github.com/ajperkins92/Project-3/master/client/public/images/seattlePark2.jpg"} style={{ width: "100%" }} />
+                                        </div>
+                                        <br />
+                                        <div className="card-content">
+                                            <div className="row">
+                                                <div className="input-field col s12">
+                                                    <input id="username" type="text" className="validate" />
+                                                    <label for="username">Username</label>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="input-field col s12">
+                                                    <input id="password" type="password" className="validate" />
+                                                    <label for="password">Password</label>
+                                                </div>
+                                            </div>
+                                            <a className="waves-effect waves-light btn green pulse" id="login">Log In</a>
                                         </div>
                                     </div>
-                                    <div className="row">
-                                        <div className="input-field col s12">
-                                            <input id="password" type="password" className="validate" />
-                                            <label for="password">Password</label>
-                                        </div>
-                                    </div>
-                                    <a className="waves-effect waves-light btn green pulse" id="login">Log In</a>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
-        </div>
+
+
+        </div >
     )
 }
 
