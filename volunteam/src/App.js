ba5835a0d5c123b60030810c12c774024869b11a
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-07 21:30:56 -0800

    Nails down Nav route to page component for MyAccount

--- volunteam/src/App.js
+++ volunteam/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Main from "./pages/main";
 import CreateEvent from "./pages/createevent";
+import MyEvents from "./pages/myevents"
+import MyAccount from "./pages/myaccount"
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -13,18 +15,18 @@ function App() {
           <Switch>
             <Route exact path="/" component={Main} />
             <Route exact path="/createevent" component={CreateEvent} />
-            {/* <Route exact path="/createevent" component={CreateEvent} />
-<Route exact path="/login" component={Login}
-<Route exact path="/logout" component={Logout}
-<Route exact path="/signup" component={SignUp}
-<Route exact path="/myevents" component={MyEvents}
-<Route exact path="/myaccount" component={MyAccount}
-<Route exact path="/event/:id" component={Event}
-            <Route component={PageNotFound} /> */}
+            <Route exact path="/createevent" component={CreateEvent} />
+            {/* <Route exact path="/login" component={Login} /> */}
+            {/* <Route exact path="/logout" component={Logout} /> */}
+            {/* <Route exact path="/signup" component={SignUp} /> */}
+            <Route exact path="/myevents" component={MyEvents} />
+            <Route exact path="/myaccount" component={MyAccount} />
+            {/* <Route exact path="/event/:id" component={Event} /> */}
+            {/* <Route component={PageNotFound} /> */}
           </Switch>
         </div>
       </Router>
     </div>
-  );
-}
-export default App;
+      );
+    }
+    export default App;
