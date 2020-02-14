ed7f7804c4c99c9b23583062ad83274b793d6e92
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-13 19:32:19 -0800

    Switches out <a> tags for <Link> components in React

--- client/src/components/mainpage/nav.js
+++ client/src/components/mainpage/nav.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 import "./nav.css";
 
 function Nav(props) {
-
-    return (
-        <nav>
-            <div className="nav-wrapper indigo">
-                <a href="/" className="brand-logo left">VolunTeam!</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {/* Logging out sets state to Logged Out, and also directs you to home page */}
-                    <li onClick={props.manageLogin}><a href={(props.loggedIn === "true") ? "/" : "/loginpage"}>{(props.loggedIn === "true") ? "Log Out" : "Log In"}</a></li>
-                    <li className={(props.loggedIn === "true") ? "hidden" : ""}><a href="/signup">Sign Up!</a></li>
-                    <li className={(props.loggedIn === "true") ? "" : "hidden"}><a href="/myevents">My Events</a></li>
-                    <li className={(props.loggedIn === "true") ? "" : "hidden"}><a href="/myaccount">My Account</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="nav-wrapper indigo">
+        <a href="/" className="brand-logo left">
+          VolunTeam!
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          {/* Logging out sets state to Logged Out, and also directs you to home page */}
+          <li onClick={props.manageLogin}>
+            <Link href={props.loggedIn === "true" ? "/" : "/loginpage"}>
+              {props.loggedIn === "true" ? "Log Out" : "Log In"}
+            </Link>
+          </li>
+          <li className={props.loggedIn === "true" ? "hidden" : ""}>
+            <Link href="/signup">Sign Up!</Link>
+          </li>
+          <li className={props.loggedIn === "true" ? "" : "hidden"}>
+            <Link href="/myevents">My Events</Link>
+          </li>
+          <li className={props.loggedIn === "true" ? "" : "hidden"}>
+            <Link href="/myaccount">My Account</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
