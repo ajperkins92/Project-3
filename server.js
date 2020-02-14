ba0f8f09743beba907b4121f4df0e0db0ccc5e84
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-13 18:59:39 -0800

    Throws all contents of 'Server' directory into the root directory and re-runs npm install

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ const app = express();
 const routes = require("./routes");
 const session = require("express-session");
 const passport = require("./passport");
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require("connect-mongo")(session);
 const morgan = require("morgan");
 const PORT = process.env.PORT || 3001;
 
 // Connect to the Mongo DB
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/volunteam";
-mongoose.connect(MONGODB_URI);
-
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true },
+  console.log("Connected to MongoDB!")
+);
 
 // Define middleware here
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,21 +31,20 @@ if (process.env.NODE_ENV === "production") {
 app.use(
   session({
     secret: "keyboard cat",
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    }),
     resave: false,
     saveUninitialized: false
-  }));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(routes);
 
-
-
-
 // Start the server
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`🌎  ==> Server now listening on PORT ${PORT}!`);
-
 });
