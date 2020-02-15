8f75598427a07a762b6a154c543f4a3978598e83
Tyler Shuck <tshuck88@gmail.com>
2020-02-15 13:50:11 -0800

    created cloudinary config folder, updated user model to include image url and ids

---
+++ cloudinary/cloudinary.js
@@ -0,0 +1,26 @@
+//  using multer and cloudinary to store user uploaded images
+const cloudinary = require("cloudinary");
+const multer = require("multer");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const dotenv = require('dotenv');
+dotenv.config()
+
+// load in cloudinary environment variables
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+// configure the cloudinary storage, only accepts jpg and png files
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "VolunTeam",
+    allowedFormats: ["jpg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }]
+});
+
+// sets multers upload to the cloudinary storage
+const parser = multer({ storage: storage });
+
+module.exports = parser;
