549adb60a5c23e04ba8b49f6a9ddd5768f138370
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-15 13:16:07 -0800

    Modifies createcard.js so that it is less ugly and displays useful information to the user when clicked on

--- client/src/components/mainpage/createcard.js
+++ client/src/components/mainpage/createcard.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CreateCard(props) {
-
-    return (
-        <div className="col">
-            <div className="card">
-                <div className="card-image waves-effect waves-block waves-light">
-                    <img className="activator" src="https://image.flaticon.com/icons/svg/892/892926.svg"></img>
-                </div>
-                <div className="card-content">
-                    <span className="card-title activator grey-text text-darken-4">Create Event<i
-                        className="material-icons right">more_vert</i></span>
-                    <p><Link to="/createevent">Create an Event</Link></p>
-                </div>
-                <div className="card-reveal">
-                    <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                </div>
-            </div>
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="card-image waves-effect waves-block waves-light">
+          <img
+            className="activator"
+            src="https://image.flaticon.com/icons/svg/892/892926.svg"
+          ></img>
         </div>
-    )
+        <div className="card-content">
+          <span className="card-title activator grey-text text-darken-4">
+            Create Event<i className="material-icons right">More info!</i>
+          </span>
+          <p>
+            <Link to="/createevent">Create an Event</Link>
+          </p>
+        </div>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4">
+            Create Event<i className="material-icons right">Close</i>
+          </span>
+          <p>
+            If you have an idea for a volunteer event in your local community,
+            click<Link to="/createevent"> here </Link>
+            to be directed to a page where you can input your event information!
+            You will show up as the event organizer when you click on that
+            event's information, and can edit the event after it's created!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateCard;
