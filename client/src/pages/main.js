b1da0cdba77948a290cd098e618d9c62ad4b0cab
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-02-20 01:28:42 -0800

    trying to get the background image to add still - also playing with container class on index.css

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -8,7 +8,7 @@ import Carousel from "../components/mainpage/carousel"
 import axios from "axios"
 import OurModal from "../components/SignUpPage/modal"
 import { Link, withRouter } from "react-router-dom";
-import "../components/mainpage/main.css"
+import "./main.css"
 
 class Main extends React.Component {
 
