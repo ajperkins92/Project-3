0a74e59eaba6007e441650b29a2a7a5999c50e7f
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2020-02-20 23:35:17 -0800

    revision of othercards.js

--- client/src/components/mainpage/othercards.js
+++ client/src/components/mainpage/othercards.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import "./othercards.css"
 
 function OtherCards(props) {
 
     return (
-        <div className="col" >
+        <div className="col s12 m6 l4 xl3">
             <div className="card">
                 <div className="card-image waves-effect waves-block waves-light">
                     <img className="activator" src={props.image}></img>
@@ -14,10 +14,10 @@ function OtherCards(props) {
                 <div className="card-content">
                     <span className="card-title activator grey-text text-darken-4">{props.eventName}</span>
                     <span>{props.date}</span>
-                        {/* href pointing to a dynamic route (on react Router, need to specify a catch all like /event/*, so that
+                    {/* href pointing to a dynamic route (on react Router, need to specify a catch all like /event/*, so that
                             whatever the id is, the /event page is still rendered, but the param does not affect the page, just the props ) */}
                     <p><Link to={`view/event/${props.eventID}`}>{props.eventName}!</Link></p>
-                   
+
                 </div>
                 <div className="card-reveal">
                     <span className="card-title grey-text text-darken-4">{props.eventName}</span>
@@ -25,7 +25,7 @@ function OtherCards(props) {
                 </div>
             </div>
         </div>
-    
+
     )
 }
 
